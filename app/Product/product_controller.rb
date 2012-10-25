0b575a95332afe526a59760338ab00545002836c
Derek Warren <derek.sa.warren@gmail.com>
2012-10-25 11:58:02 -0400

    Picture taking procedures updated

--- app/Product/product_controller.rb
+++ app/Product/product_controller.rb
@@ -12,14 +12,29 @@ class ProductController < Rho::RhoController
   end
 
   # GET /Product/{1}
-  def show
-    
-    #Add picture dectection logic here
-    #Prompt to take picture if one isn't present
-    
+  def show  
     @product = Product.find(@params['id'])
+    flag = @params['flag']
+      
+    # Does a product exist?
     if @product
-      render :action => :show, :back => url_for(:action => :index)
+      # Is there an image for the product?
+      if @product.image == ""
+        # Have we prompted to take an image previously? If 'YES' (1) then just show page;
+        # otherwise, Alert user if they would like to take a picture.
+        if flag == '1'
+          render :action => :show, :back => url_for(:action => :index)
+        else
+          Alert.show_popup( {
+              :message => "Would you like to add a picture?",
+              :title => "Add Picture?",
+              :buttons => ["Yes", "No"],
+              :callback => url_for(:action => :handle_picture, :id => @product.object)
+          } )
+        end
+      else
+        render :action => :show, :back => url_for(:action => :index)
+      end
     else
       redirect :action => :index
     end
@@ -144,11 +159,18 @@ class ProductController < Rho::RhoController
   ## Partial: MC55 - Camera starts but data is not passed.
   ## Failing: MC65 - Camera doesn't startup.
   
+  def handle_picture
+    button = @params['button_id']
+    objectID = @params['id']
+    # If the user selected 'Yes' we will take a picture
+    if button=='Yes'
+      WebView.navigate url_for(:action => :camera_picture, :id => objectID)
+    end
+  end
+  
   # GET /Product/{1}/picture
   def picture
     @product = Product.find(@params['id'])
-    puts "!!!!!!!!!!!!!!!!!!!!! #{@product}"
-    $placehold = @product
     if @product
       render :action => :picture, :back => url_for(:action => :index)
     else
@@ -156,61 +178,69 @@ class ProductController < Rho::RhoController
     end
   end
   
-  def display_picture
+  # Start the camera to take a picture of the item
+  def camera_picture
+      objectID = @params['id']
       puts "!!!!!!!!!!!!!!!!!PICTURE!!!!!!!!!!!!!!!!!!"
       puts Camera::get_camera_info('main')
-      puts Camera::get_camera_info('default')
-      Camera::take_picture(url_for(:action => :camera_callback))
-  end
-  
-  def detect_camera
-      if !System::get_property('has_camera')
-        Alert.show_popup(
-          :message => 'This Device does not have a Camera',
-          :title => 'NO CAMERA',
-          :buttons => ['ok'],
-          :callback => url_for(:action => :index_callback)
-        )
-        render :back => '/app/product'
-      else
-        Alert.show_popup(
-               :message => 'This Device HAS a Camera!!',
-               :title => 'YES CAMERA',
-               :buttons => ['ok'],
-               :callback => url_for(:action => :index_callback)
-        )
-        render :action => :camera_default
-        #Camera::take_picture(url_for(:action => :camera_callback))
-      end
+      Camera::take_picture(url_for(:action => :camera_callback, :id => objectID))
   end
   
   # Save the Picture to display for the Product
   def camera_callback
+    @product = Product.find(@params['id'])
+    # When picture was taken successfully update the product w/ image
     if @params['status'] == 'ok'
-      puts "ENTER CALLBACK WITH THIS PRODUCT ID!!!!!!!!!!!! #{$placehold}"
-      #puts "THIS IS THE LOCATION OF THE IMAGE!!!!!! #{@params}"
+      puts "ENTER CALLBACK WITH THIS PRODUCT ID!!!!!!!!!!!! #{@product}"
       
       #This is taking the picture and encoding it in base64 to display on device and upload if needed.
       #The io portion is taking the opened inmage and converting it to a String.
       #temp = Base64.encode64(open(@params['image_uri']) {|io| io.read})
       #puts "!~~~!~!~!~!~!~!~!~!~!~!~!~! #{temp}"
-
       #$placehold.update_attributes({"image" => "data:image/jpeg;base64," + temp.to_s })
-      $placehold.update_attributes({"image" => @params['image_uri']})
-      #puts "CHECK UPDATE!!!!!!! #{$placehold}"
+      
+      @product.update_attributes({"image" => @params['image_uri']})
     else
       Alert.show_popup(
         :message => @params['status'],
         :title => 'ERROR',
         :buttons => ['ok'],
-        :callback => url_for(:action => :index_callback)
+        :callback => url_for(:action => :show_callback)
       )
     end
-    WebView.navigate(url_for :action => :picture, :id => $placehold.object)
+    #Return to the show page
+    WebView.navigate(url_for :action => :show, :id => @product.object, :flag => '1')
+  end
+  
+  def detect_camera
+    if !System::get_property('has_camera')
+      Alert.show_popup(
+        :message => 'This Device does not have a Camera',
+        :title => 'NO CAMERA',
+        :buttons => ['ok'],
+        :callback => url_for(:action => :index_callback)
+      )
+      render :back => '/app/product'
+    else
+      Alert.show_popup(
+             :message => 'This Device HAS a Camera!!',
+             :title => 'YES CAMERA',
+             :buttons => ['ok'],
+             :callback => url_for(:action => :index_callback)
+      )
+      render :action => :camera_default
+      #Camera::take_picture(url_for(:action => :camera_callback))
+    end
   end
   
   def index_callback
+    #redirect :action => :index
     WebView.navigate url_for(:action => :index)
+  end
+  
+  def show_callback
+    redirect :action => :show
+    #WebView.navigate url_for(:action => :show, :id => objectID)
   end
   
 end
